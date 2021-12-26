7b233eeac7a8e2cc721a56ab0926559c2a03c0d7
placidohoff <67982091+placidohoff@users.noreply.github.com>
2021-12-26 10:25:54 -0500

    Routing works for campsites + comments

--- routes/campsiteRouter.js
+++ routes/campsiteRouter.js
@@ -1,54 +1,238 @@
 const express = require('express')
 const campsiteRouter = express.Router()
+const Campsite = require('../models/campsite')
 
 //The full root path is provided within server.js:
 //Route chain, one single statement containing each route:
 //We no longer have to specify the full full name, it was set for us in server.js and we can reference that as '/'
 campsiteRouter.route('/')
-    .all((req, res, next) => {
-        res.statusCode = 200
-        res.setHeader('Content-Type', 'text/plain')
-        //forwards this request to the next relevent middlware:
-        //If is a 'get', will forward to the next 'get', if is a 'post', etc:
-        next()
-    })
     .get((req, res) => {
-        res.end('Will send all the campsites to you')
+        Campsite.find()
+            .then(campsites => {
+                res.statusCode = 200
+                res.setHeader('Content-Type', 'application/json')
+                res.json(campsites)
+            })
+            .catch(err => next(err))
     })
-    .post((req, res) => {
-        res.end(`Will add the campsite: ${req.body.name} with description: ${req.body.description}`)
+    .post((req, res, next) => {
+        //Mongoose will automatically validate if the req.body from the user is valid according to our Mongoose Schema
+        Campsite.create(req.body)
+            .then(campsite => {
+                console.log('Campsite Created ', campsite)
+                res.statusCode = 200
+                res.setHeader('Content-Type', 'application/json')
+                res.json(campsite)
+            })
+            .catch(err => next(err))
     })
     .put((req, res) => {
         res.statusCode = 403
         res.end('PUT operation not supported on /campsites')
     })
-    .delete((req, res) => {
-        res.end('Deleting all campsites')
+    .delete((req, res, next) => {
+        Campsite.deleteMany()
+            .then(response => {
+                res.statusCode = 200
+                res.setHeader('Content-Type', 'application/json')
+                res.json(response)
+            })
+            .catch(err => next(err))
     })
 
 campsiteRouter.route('/:campsiteId')
-    .all((req, res, next) => {
-        res.statusCode = 200
-        res.setHeader('Content-Type', 'text/plain')
-        //forwards this request to the next relevent middlware:
-        //If is a 'get', will forward to the next 'get', if is a 'post', etc:
-        next()
-    })
-    .get((req, res) => {
-        res.end(`Will send details of the campsite: ${req.params.campsiteId} to you`)
+    .get((req, res, next) => {
+        Campsite.findById(req.params.campsiteId)
+            .then(campsite => {
+                res.statusCode = 200
+                res.setHeader('Content-Type', 'application/json')
+                res.json(campsite)
+            })
+            .catch(err => next(err))
     })
     .post((req, res) => {
         res.status = 403
         res.end(`POST operation not supported on /campsites/${req.params.campsiteId}`)
     })
     .put((req, res) => {
-        res.write(`Updating the campsite: ${req.params.campsiteId}\n`)
-        res.end(`Will update the campsite ${req.body.name}
-        with description: ${req.body.description}`)
-        //^^Multi-line response returned as text
+        Campsite.findByIdAndUpdate(req.params.campsiteId, {
+            $set: req.body
+        }, { new: true })
+            .then(campsite => {
+                res.statusCode = 200
+                res.setHeader('Content-Type', 'application/json')
+                res.json(campsite)
+            })
+            .catch(err => next(err))
+            .delete((req, res, next) => {
+                Campsite.findByIdAndDelete(req.params.campsiteId)
+                    .then(response => {
+                        res.statusCode = 200
+                        res.setHeader('Content-Type', 'application/json')
+                        res.json(response)
+                    })
+                    .catch(err => next(err))
+            })
     })
-    .delete((req, res) => {
-        res.end(`Deleting campsite: ${req.params.campsiteId}`)
+
+campsiteRouter.route('/:campsiteId/comments')
+    .get((req, res, next) => {
+        Campsite.findById(req.params.campsiteId)
+            .then(campsite => {
+                if (campsite) {
+
+
+                    res.statusCode = 200
+                    res.setHeader('Content-Type', 'application/json')
+                    res.json(campsite.comments)
+
+                } else {
+                    err = new Error(`Campsite ${req.params.campsiteId} not found`)
+                    err.status = 404
+                    return next(err)
+                }
+            })
+            .catch(err => next(err))
+    })
+    .post((req, res, next) => {
+        //Mongoose will automatically validate if the req.body from the user is valid according to our Mongoose Schema
+        Campsite.findById(req.params.campsiteId)
+            .then(campsite => {
+                if (campsite) {
+                    campsite.comments.push(req.body)
+
+                    //save to modified object to MongoDB:
+                    campsite.save()
+                        .then(campsite => {
+                            res.statusCode = 200
+                            res.setHeader('Content-Type', 'application/json')
+                            res.json(campsite.comments)
+                        })
+                        .catch(err => next(err))
+                } else {
+                    err = new Error(`Campsite ${req.params.campsiteId} not found`)
+                    err.status = 404
+                    return next(err)
+                }
+            })
+            .catch(err => next(err))
+    })
+    .put((req, res) => {
+        res.statusCode = 403
+        res.end(`PUT operation not supported on /campsites/${req.params.campsiteId}/comments`)
+    })
+    .delete((req, res, next) => {
+        Campsite.findById(req.params.campsiteId)
+            .then(campsite => {
+                if (campsite) {
+                    for (let i = (campsite.comments.length - 1); i >= 0; i--) {
+                        campsite.comments.id(campsite.comments[i]._id).remove()
+                    }
+                    //save to modified object (deleted comments) to MongoDB:
+                    campsite.save()
+                        .then(campsite => {
+                            res.statusCode = 200
+                            res.setHeader('Content-Type', 'application/json')
+                            res.json(campsite.comments)
+                        })
+                        .catch(err => next(err))
+                } else {
+                    err = new Error(`Campsite ${req.params.campsiteId} not found`)
+                    err.status = 404
+                    return next(err)
+                }
+            })
+    })
+
+//Specific comment of a specific campsite:
+campsiteRouter.route('/:campsiteId/comments/:commentId')
+    .get((req, res, next) => {
+        Campsite.findById(req.params.campsiteId)
+            .then(campsite => {
+                if (campsite && campsite.comments.id(req.params.commentId)) {
+
+
+                    res.statusCode = 200
+                    res.setHeader('Content-Type', 'application/json')
+                    res.json(campsite.comments)
+
+                } else if (!campsite) {
+                    err = new Error(`Campsite ${req.params.campsiteId} not found`)
+                    err.status = 404
+                    return next(err)
+                }
+                else {
+                    err = new Error(`Comment ${req.params.campsiteId} not found`)
+                    err.status = 404
+                    return next(err)
+                }
+            })
+            .catch(err => next(err))
+    })
+    .post((req, res) => {
+        res.statusCode = 403
+        res.end(`POST operation not supported on /campsites/${req.params.campsiteId}/comments/${req.params.commentId}`)
+    })
+    .put((req, res) => {
+        Campsite.findById(req.params.campsiteId)
+            .then(campsite => {
+                if (campsite && campsite.comments.id(req.params.commentId)) {
+                    if (req.body.rating) {
+                        campsite.comments.id(req.params.commentId).rating = req.body.rating
+                    }
+                    if (req.body.text) {
+                        campsite.comments.id(req.params.commentId).text = req.body.text
+                    }
+
+                    //Save updated comment to the MongoDB:
+                    campsite.save()
+                        .then(campsite => {
+                            res.statusCode = 200
+                            res.setHeader('Content-Type', 'application/json')
+                            res.json(campsite)
+                        })
+                        .catch(err => next(err))
+
+                } else if (!campsite) {
+                    err = new Error(`Campsite ${req.params.campsiteId} not found`)
+                    err.status = 404
+                    return next(err)
+                }
+                else {
+                    err = new Error(`Comment ${req.params.campsiteId} not found`)
+                    err.status = 404
+                    return next(err)
+                }
+            })
+            .catch(err => next(err))
+    })
+    .delete((req, res, next) => {
+        Campsite.findById(req.params.campsiteId)
+            .then(campsite => {
+                if (campsite && campsite.comments.id(req.params.commentId)) {
+                    campsite.comments.id(req.params.commentId).remove()
+
+                    //Save updated comment to the MongoDB:
+                    campsite.save()
+                        .then(campsite => {
+                            res.statusCode = 200
+                            res.setHeader('Content-Type', 'application/json')
+                            res.json(campsite)
+                        })
+                        .catch(err => next(err))
+
+                } else if (!campsite) {
+                    err = new Error(`Campsite ${req.params.campsiteId} not found`)
+                    err.status = 404
+                    return next(err)
+                }
+                else {
+                    err = new Error(`Comment ${req.params.campsiteId} not found`)
+                    err.status = 404
+                    return next(err)
+                }
+            })
+
     })
 
 
