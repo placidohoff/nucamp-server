120e121a5fbc825d7e5f6a45cb9f6b4e54c4ed6e
placidohoff <67982091+placidohoff@users.noreply.github.com>
2022-01-01 01:18:25 -0500

    Adding basic cookie for auth

--- app.js
+++ app.js
@@ -33,35 +33,51 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+//We are providing a 'secret key' to our cookie's making them "signed-cookies" so we can validate if the cookie returned actually came from our server
+app.use(cookieParser('12345-67890-09876-54321'));
 
 //AUTHENTICATION PROCESS:
-//PLACED BEFORE RETURNING ANY STATIC INFO OR ANY INFO AT ALL IN THIS CASE
 //WE ARE JUST DEFINING THE FUNCTION HERE. IT IS NOT CALLED JUST YET
 function auth(req, res, next) {
   console.log(req.headers)
-  const authHeader = req.headers.authorization
-  if (!authHeader) {
-    const err = new Error('You are not authenticated!')
-    //response sent back to user letting the client's browser know that they must provide authentication by setting the authentication to 'Basic'
-    res.setHeader('WWW-Authenticate', 'Basic')
-    err.status = 401
-    return next(err)
+  //If we cookie, request/challenge the user to sign in
+  if (!req.signedCookies.user) {
+    const authHeader = req.headers.authorization
+    if (!authHeader) {
+      const err = new Error('You are not authenticated!')
+      //response sent back to user letting the client's browser know that they must provide authentication by setting the authentication to 'Basic'
+      res.setHeader('WWW-Authenticate', 'Basic')
+      err.status = 401
+      return next(err)
+    }
+
+    //if an autherization is provided:
+    //Separate the password and user from the header via split methods on the string provided:
+    const auth = Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':')
+    const user = auth[0]
+    const pass = auth[1]
+
+    if (user === 'admin' && pass === 'password') {
+      //Set cookie so that future challenges will not happen for this session:
+      res.cookie('user', 'admin', { signed: true })
+      //Proceed to next middleware:
+      return next() //authorization success
+    } else {
+      const err = new Error('You are not authenticated')
+      res.setHeader('WWW-Authenticate', 'Basic')
+      err.status = 401
+      return next(err)
+    }
   }
-
-  //if an autherization is provided:
-  //Separate the password and user from the header via split methods on the string provided:
-  const auth = Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':')
-  const user = auth[0]
-  const pass = auth[1]
-
-  if (user === 'admin' && pass === 'password') {
-    return next() //authorization success
-  } else {
-    const err = new Error('You are not authenticated')
-    res.setHeader('WWW-Authenticate', 'Basic')
-    err.status = 401
-    return next(err)
+  //IF THERE IS INDEED A COOKIE:
+  else {
+    if (req.signedCookies.user === 'admin') {
+      return next()
+    } else {
+      const err = new Error('You are not authenticated')
+      err.status = 401
+      return next(err)
+    }
   }
 
 }
